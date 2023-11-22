2a7ff6d63386ea0ec04cc162cbe059f05f31e6d9
Eoin Lyng <elyng@carlowcoco.ie>
2023-11-22 12:24:11 +0000

    Signed-off-by: Eoin Lyng <elyng@carlowcoco.ie>

--- udp_client1.py
+++ udp_client1.py
@@ -21,7 +21,7 @@ while True:
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         #list include letters and numbers to simulate error log with random letter instead of integer
-        temp_list = [-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,18,20,21,23,24,25,26,27,28,29,30,31,35,32,33,34,'a','b','c','d']
+        temp_list = [-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,18,20,21,23,24,25,26,27,28,29,30,31,35,32,33,34,35,'a','b','c','d']
         #create random range between -5 and 35 degrees https://www.w3schools.com/python/ref_random_randrange.asp
         message_text = f"SensorOne,Degrees Temperature in Celsius,{(random.choice(temp_list))},{datetime.now()}"
         message = message_text.encode('utf-8')
